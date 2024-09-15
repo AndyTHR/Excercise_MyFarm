53d640086bc8086b6fc007a4ed0b8910507b9322
Andy THR <trunghauphan2710@gmail.com>
2024-09-15 12:49:04 +0700

    update 10

--- Assets/Scripts/DayAndNightManager.cs
+++ Assets/Scripts/DayAndNightManager.cs
@@ -13,6 +13,11 @@ public class DayAndNightManager : MonoBehaviour
     {
         DateTime realtime = DateTime.Now;
         float realSecondInDay = (realtime.Hour * 3600) + (realtime.Minute * 60) + realtime.Second;
+        float gameTimeSeconds = (realSecondInDay/ (24*3600)) * (dayDuaration * 60);
+        int gameHour = Mathf.FloorToInt(gameTimeSeconds / (dayDuaration * 60) * 24);
+        int gameMinutes = Mathf.FloorToInt((gameTimeSeconds / (dayDuaration * 60) * 24 * 60) % 60);
+        string timeformatted = string.Format("{0:00}:{ 1:00}", gameHour, gameMinutes);
+        textTimeInGame.text = timeformatted;
     }
 
 }
