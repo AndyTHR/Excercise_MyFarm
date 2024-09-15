6b0865393a044fbfa071b6b607315773c449a36d
Andy THR <trunghauphan2710@gmail.com>
2024-09-15 13:09:42 +0700

    update 11

--- Assets/Scripts/DayAndNightManager.cs
+++ Assets/Scripts/DayAndNightManager.cs
@@ -2,22 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class DayAndNightManager : MonoBehaviour
 {
     public Text textTimeInGame;
-    public float dayDuaration = 5f;
+    public float dayMultiplier = 20;
+    public Light2D Light2D;
+    public Gradient gradient;
 
     private void Update()
     {
         DateTime realtime = DateTime.Now;
         float realSecondInDay = (realtime.Hour * 3600) + (realtime.Minute * 60) + realtime.Second;
-        float gameTimeSeconds = (realSecondInDay/ (24*3600)) * (dayDuaration * 60);
-        int gameHour = Mathf.FloorToInt(gameTimeSeconds / (dayDuaration * 60) * 24);
-        int gameMinutes = Mathf.FloorToInt((gameTimeSeconds / (dayDuaration * 60) * 24 * 60) % 60);
-        string timeformatted = string.Format("{0:00}:{ 1:00}", gameHour, gameMinutes);
+        realSecondInDay = (realSecondInDay * dayMultiplier) % 86400;
+        //float gameTimeSeconds = (realSecondInDay/ (24*3600)) * (dayDuaration * 60);
+        int gameHour = Mathf.FloorToInt(realSecondInDay / 3600);
+        int gameMinutes = Mathf.FloorToInt((realSecondInDay * 60)/ 60);
+        string timeformatted = string.Format($"{0:00}:{ 1:00}", gameHour, gameMinutes);
         textTimeInGame.text = timeformatted;
+
+        ChangeColorByTime(realSecondInDay);
+    }
+
+    public void ChangeColorByTime(float seconds)
+    {
+        Light2D.color = gradient.Evaluate(seconds / 86400);
     }
 
 }
